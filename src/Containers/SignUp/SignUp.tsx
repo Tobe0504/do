a9dfd366af0d5788bfb336d625b6170ed0b68603
Tobe0504 <ezimorahtobenna@gmail.com>
2024-09-27 20:12:24 +0100

    removed all users data upon sign up

--- src/Containers/SignUp/SignUp.tsx
+++ src/Containers/SignUp/SignUp.tsx
@@ -4,6 +4,7 @@ import Button from "../../Components/Button/Button";
 import Input from "../../Components/Input/Input";
 import OnboardingLayout from "../../Components/OnboardingLayout/OnboardingLayout";
 import { AuthUserContext } from "../../Context/AuthUserContext";
+import { getLocalStorage } from "../../HelperFunctions/decryptData";
 import Logo from "../Logo/Logo";
 import classes from "../SignIn/SignIn.module.css";
 
@@ -64,6 +65,10 @@ const SignUp = () => {
             onClick={(e) => {
               e.preventDefault();
               signUp();
+              localStorage.removeItem("do-todos");
+              localStorage.removeItem("do-user");
+              localStorage.removeItem("do-user-state");
+              localStorage.removeItem("summary");
             }}
             disabled={
               !loginDetails.email ||
